7b0f27eb606e6c898227e5ff857a35403ca1cd6a
santosh-burada <santuburada1999@gmail.com>
2020-12-14 20:26:05 +0530

    Live Camera Function Added, But Testing is running

--- ImageRecognition/ImageMain.py
+++ ImageRecognition/ImageMain.py
@@ -53,64 +53,69 @@ if n:
     with open("E:\PycharmProjects\/attendence\imgNames.txt", "wb") as fp:  # Pickling
         pickle.dump(imgNames, fp)
 else:
-    imgPath = input("Give the Path of the Input Image : ")
-    imgInput = face_recognition.load_image_file(imgPath)
-    imgInput = cv2.resize(imgInput, (0, 0), None, 0.25, 0.25)
-    imgInput = cv2.cvtColor(imgInput, cv2.COLOR_BGR2RGB)
-    # cv2.imshow("input image", imgInput)
-    try:
+    cap = cv2.VideoCapture(0)
+    while True:
+        success,img = cap.read()
 
-        faceLocation = face_recognition.face_locations(imgInput)
-        print(faceLocation, "input face location")
-        inputFeatures = face_recognition.face_encodings(imgInput, faceLocation, model='cnn')
-        print("saomething")
-        # Here we wil have the input face features
-        with open("featuresOfTrainingImages.txt", "rb") as fp:  # Unpickling
-            featuresOfTrainingImages = pickle.load(fp)
-        with open("imgNames.txt", "rb") as fp:
-            imgNames = pickle.load(fp)
-        with open("images.txt", "rb") as fp:
-            images = pickle.load(fp)
-        # matching the input feature with the loaded images features.
-        attendance = []
-        for encodeInput, facesOfInput in zip(inputFeatures, faceLocation):
-            # we don't require matches we take distance as first preference for accuracy.
-            # matchs = face_recognition.compare_faces(featuresOfTrainingImages, encodeInput, tolerance=0.3)
-            # print(encodeInput)
-            faceDistance = face_recognition.face_distance(featuresOfTrainingImages, encodeInput)
+        imgInput = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgInput = cv2.cvtColor(imgInput, cv2.COLOR_BGR2RGB)
+        cv2.imshow("input", imgInput)
 
-            print(imgNames, "names")
-            print(faceDistance, "facedistance")
-            index = np.argmin(faceDistance)
-            print(imgNames[index])
-            attendance.append(imgNames[index])
-        # cv2.imwrite(os.path.join(finalPath, 'InputImage.jpg'), imgInput)
-        #
-        # for i in range(len(faceDistance)):
-        #     if faceDistance[i] == faceDistance[index]:
-        #         print(imgNames[i])
-        # for i in range(len(matchs)):
-        #     if matchs[i]:
-        #         print(i, matchs[i])
-        #         cv2.imwrite(os.path.join(finalPath, 'MatchedImage' + str(i) + '.jpg'), images[i])
-        #         name = imgNames[i].lower()
-        #         flag = 0
-        #         print(name)
-        # if flag:
-        #     flag = 0
-        #     c = 1
-        #     print("Closely related face")
-        #     for i in range(len(faceDistance)):
-        #         if faceDistance[i] <= faceDistance[index]:
-        #             c = 0
-        #             print(imgNames[i])
-        #             cv2.imwrite(os.path.join(finalPath, 'CloselyMatchedImage' + str(i) + '.jpg'), images[i])
-        #     if c:
-        #         print("No Face are Matched With your Input Image")
-        #
-        # if flag:
-        #     print("No Face are Matched With your Input Image")
-    except Exception as e:
-        print(f"We are sorry! Unable to find the Faces {e}")
+        # cv2.imshow("input image", imgInput)
+        try:
+
+            faceLocation = face_recognition.face_locations(imgInput)
+            if len(faceLocation)>0:
+
+                inputFeatures = face_recognition.face_encodings(imgInput, faceLocation, model='cnn')
+                # Here we wil have the input face features
+                with open("featuresOfTrainingImages.txt", "rb") as fp:  # Unpickling
+                    featuresOfTrainingImages = pickle.load(fp)
+                with open("imgNames.txt", "rb") as fp:
+                    imgNames = pickle.load(fp)
+                with open("images.txt", "rb") as fp:
+                    images = pickle.load(fp)
+                # matching the input feature with the loaded images features.
+                attendance = []
+                for encodeInput, facesOfInput in zip(inputFeatures, faceLocation):
+                    # we don't require matches we take distance as first preference for accuracy.
+                    # matchs = face_recognition.compare_faces(featuresOfTrainingImages, encodeInput, tolerance=0.3)
+                    # print(encodeInput)
+                    faceDistance = face_recognition.face_distance(featuresOfTrainingImages, encodeInput)
+
+                    print(imgNames, "names")
+                    print(faceDistance, "facedistance")
+                    index = np.argmin(faceDistance)
+                    print(imgNames[index])
+                    if imgNames not in attendance:
+                        attendance.append(imgNames[index])
+            # cv2.imwrite(os.path.join(finalPath, 'InputImage.jpg'), imgInput)
+            #
+            # for i in range(len(faceDistance)):
+            #     if faceDistance[i] == faceDistance[index]:
+            #         print(imgNames[i])
+            # for i in range(len(matchs)):
+            #     if matchs[i]:
+            #         print(i, matchs[i])
+            #         cv2.imwrite(os.path.join(finalPath, 'MatchedImage' + str(i) + '.jpg'), images[i])
+            #         name = imgNames[i].lower()
+            #         flag = 0
+            #         print(name)
+            # if flag:
+            #     flag = 0
+            #     c = 1
+            #     print("Closely related face")
+            #     for i in range(len(faceDistance)):
+            #         if faceDistance[i] <= faceDistance[index]:
+            #             c = 0
+            #             print(imgNames[i])
+            #             cv2.imwrite(os.path.join(finalPath, 'CloselyMatchedImage' + str(i) + '.jpg'), images[i])
+            #     if c:
+            #         print("No Face are Matched With your Input Image")
+            #
+            # if flag:
+            #     print("No Face are Matched With your Input Image")
+        except Exception as e:
+            print(f"We are sorry! Unable to find the Faces {e}")
 
 cv2.waitKey(0)
